0b4edc00f96f5f78f2abe1c22d0129dcb6b0e124
WebWizard-Dushyant <dushyantpiplani@outlook.com>
2026-04-14 12:18:12 +0530

    Add service worker event listeners for install and fetch

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
 
+self.addEventListener('fetch', (e) => {
+  // Required fetch interceptor for Chrome to approve PWA installation
+  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
+});
